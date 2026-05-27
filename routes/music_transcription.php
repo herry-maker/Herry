0c69e882b46d691e16b2a3a7f6e32cbb052befd2
herry-maker <herry@fmsp.upsi.edu.my>
2026-05-27 21:55:07 +0800

    Add music transcription API routes

---
+++ routes/music_transcription.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\MusicTranscriptionController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Music Transcription API Routes
+|--------------------------------------------------------------------------
+|
+| Routes for PDF to MuseScore music transcription feature
+|
+*/
+
+Route::prefix('api/music')->group(function () {
+    // Upload and transcribe PDF
+    Route::post('/transcribe', [MusicTranscriptionController::class, 'uploadAndTranscribe']);
+    
+    // List all transcriptions
+    Route::get('/transcriptions', [MusicTranscriptionController::class, 'listTranscriptions']);
+    
+    // Download a transcribed file
+    Route::get('/download/{file}', [MusicTranscriptionController::class, 'downloadTranscription'])
+        ->name('download-transcription');
+    
+    // Delete a transcribed file
+    Route::delete('/delete/{file}', [MusicTranscriptionController::class, 'deleteTranscription']);
+});
